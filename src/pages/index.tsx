fbef503fb18ef9b1cf19433d128638720674fc25
hieupq <hieu.phq@gmail.com>
2020-03-18 14:13:21 +0700

    Integrate tailwind and typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style/main.css'
+
+export default () => (
+  <div className="md:flex bg-white rounded-lg p-6">
+    <img
+      className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+      src="https://randomuser.me/api/portraits/women/17.jpg"
+    />
+    <div className="text-center md:text-left">
+      <h2 className="text-lg">Erin Lindford</h2>
+      <div className="text-purple-500">Customer Support</div>
+      <div className="text-gray-600">erinlindford@example.com</div>
+      <div className="text-gray-600">(555) 765-4321</div>
+    </div>
+  </div>
+)
